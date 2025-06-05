51131c69e1586aec870bcdf8ced7c3e256cbcdf7
Aks-20 <akshatgupta0043@gmail.com>
2025-06-05 18:09:06 +0530

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -57,7 +57,7 @@ const About: React.FC = () => {
               {/* Resume Download Button */}
               <div className="pt-8">
                 <a
-                  href="/public/Akshat Resume.pdf"
+                  href="/public/resume.pdf"
                   download
                   target="_blank"
                   rel="noopener noreferrer"
